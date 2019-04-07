bc1ab53b5b404fbe07beac1c2956be667d8af54d
Muhammad Amhan <muhammadamhan@gmail.com>
2019-04-07 18:48:08 +0200

    Python requests lib example.
    How to target any web content you wish.
    Retrieve the information you need from any source on the web.
    Filter and structure this data and process it in your Python project.

---
+++ web_scraper.py
@@ -0,0 +1,41 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class FetchProductInfo(object):
+
+    def __init__(self):
+        self.url = 'https://www.humblebundle.com/games/humble-indie-bundle-20?hmb_source=navbar&hmb_medium=product_tile&hmb_campaign=tile_index_1'
+        self.products_info_dict = {}
+        self.get_offers()
+
+    def get_offers(self):
+        req = requests.get(self.url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+
+        products_blocks = soup.select('.dd-game-row')
+
+        for products_block in products_blocks:
+            products_headline = products_block.select('.dd-header-headline')
+            if products_headline:
+                stripped_headline = products_headline[0].text.strip()
+                products = [product.text.strip() for product in products_block.select('.dd-image-box-caption')]
+
+                self.products_info_dict[stripped_headline] = {
+                    'products': products
+                }
+
+        self.response_to_str()
+
+    def response_to_str(self):
+        for product_headline, products_info in self.products_info_dict.items():
+            print(f'{product_headline}')
+            print('Products:')
+            print('{1} {0}'.format('\n\t* '.join(products_info['products']), '\t*'))
+            print('\n')
+
+
+if __name__ == '__main__':
+    request = FetchProductInfo()
+
